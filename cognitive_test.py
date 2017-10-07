625158b5ed84081750301d9d58435f759c42f672
Mounika1494 <edulamounika08@gmail.com>
2017-10-07 22:04:15 +0000

    Cognitive with decision

--- cognitive_test.py
+++ cognitive_test.py
@@ -2,7 +2,7 @@ import requests
 import json
 import os
 import re
-
+import numpy as np
 url_Microsoft_Cog_Services = 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?'
 Image_url = 'Anger.jpg'
 #Header for Microsoft Cognitive Services requests
@@ -28,8 +28,34 @@ response= requests.request('post',url_Microsoft_Cog_Services,json =json, data = 
 data = response.json()
 print(data)
 				#Searching for Pattern of the required items in the image response from cognitive services
-data1 = json.loads(json.dumps(data['scores'][0]['anger']))
-print(data1)
+
+array = np.zeros(8)
+array[0] = data[0]['scores']['anger']
+array[1] = data[0]['scores']['contempt']
+array[2] = data[0]['scores']['disgust']
+array[3] = data[0]['scores']['fear']
+array[4] = data[0]['scores']['happiness']
+array[5] = data[0]['scores']['neutral']
+array[6] = data[0]['scores']['sadness']
+array[7] = data[0]['scores']['surprise']
+max_value = array[0]
+max_index = 0
+
+
+for i in range(0,7):
+    if array[i]>max_value:
+        max_value = array[i]
+        max_index = i
+
+print(max_index)
+     
+    
+     
+
+#print(ang)
+#print(cont)
+#print(dis)
+
 
                 #Apple_Found = re.search('apple', str(data))
                 #Cabbage_Found = re.search('cabbage',str(data))
